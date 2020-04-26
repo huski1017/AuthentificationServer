4cad9310d4d72a49c03b728aaf126f97b8f3f1ad
HubertW <hubert.wroblewski@epitech.eu>
2020-04-26 20:47:21 +0200

    init account routes for fun

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,22 @@ app.get('/', (req, res) => {
   res.send('What up mate? Trying to know more about me?')
 });
 
+app.get('/account', (req, res) => {
+  res.status(501).send('Ooopsie not implemented yet!')
+});
+
+app.post('/account', (req, res) => {
+  res.status(501).send('Ooopsie not implemented yet!')
+});
+
+app.put('/account', (req, res) => {
+  res.status(501).send('Ooopsie not implemented yet!')
+});
+
+app.delete('/account', (req, res) => {
+  res.status(501).send('Ooopsie not implemented yet!')
+});
+
 app.listen(8000, () => {
   console.log('App listening on port 8000!')
 });
