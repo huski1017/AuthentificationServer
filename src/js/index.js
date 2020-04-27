6fc4a5dbea9c7c433483bf41099b5a5622efba8d
HubertW <hubert.wroblewski@epitech.eu>
2020-04-27 15:11:10 +0200

    Update route for users

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,20 @@
 const express = require('express')
 const app = express();
 
+var bodyParser = require('body-parser')
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+var user = require('./user.js');
+
 app.get('/', (req, res) => {
   res.send('What up mate? Trying to know more about me?')
 });
 
-app.get('/account', (req, res) => {
-  res.status(501).send('Ooopsie not implemented yet!')
-});
-
-app.post('/account', (req, res) => {
-  res.status(501).send('Ooopsie not implemented yet!')
-});
-
-app.put('/account', (req, res) => {
-  res.status(501).send('Ooopsie not implemented yet!')
-});
-
-app.delete('/account', (req, res) => {
-  res.status(501).send('Ooopsie not implemented yet!')
-});
+app.use('/user', user);
 
 app.listen(8000, () => {
   console.log('App listening on port 8000!')
